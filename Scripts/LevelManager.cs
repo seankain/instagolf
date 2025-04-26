b95bf7f593f017506aa707a2e7d190f84a8206b3
unknown <sean.kain.geo@gmail.com>
2025-04-25 22:44:17 -0400

    adding levels to specific node in scene

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -10,6 +10,9 @@ public partial class LevelManager : Node3D
     public PackedScene[] LevelScenes;
 
     [Export]
+    public Control Menu;
+
+    [Export]
     public Node3D LevelRoot;
     public override void _Ready()
     {
@@ -19,14 +22,32 @@ public partial class LevelManager : Node3D
         }
     }
 
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventKey eventKey)
+        {
+            if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
+            {
+                ToggleMenu();
+            }
+        }
+    }
+
+    private void ToggleMenu()
+    {
+        Menu.Visible = !Menu.Visible;
+    }
+
     private void AddPlayer(long peerId)
     {
         var p = GD.Load<PackedScene>(PlayerScene.ResourcePath);
         var player = p.Instantiate();
         player.Name = peerId.ToString();
         AddChild(player);
+        GD.Print("Adding player");
         foreach (var spawnNode in GetTree().GetNodesInGroup("SpawnPositions"))
         {
+            GD.Print("Spawn node");
             if (spawnNode.Name == $"spawn{player.Name}")
             {
                 GD.Print("Setting player position");
